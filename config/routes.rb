d95dfe7235601701e99e55f692bf867a37cca133
Your Name <phagmann1@gmail.com>
2016-09-03 23:09:06 +0000

    Added foo to the bar

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'quotes#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  resources :quotes
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
