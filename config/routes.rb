a759fd8fc7d1c3ff977b82ad91a7e07a14772966
Your Name <phagmann1@gmail.com>
2016-09-05 04:08:56 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :quotes
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  get 'about', to: 'quotes#about'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
